3ae747b21d75d253084dd2ee94b21c9f4d2ac571
Cami <camille@creativeambition.net>
2019-01-29 22:55:31 -0800

    data card rendered

---
+++ src/comps/InfoCard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class InfoCard extends Component {
+    render() {
+        return (
+            <div className="infoCard">
+                <p>User: James Bodwin </p>
+                {/* <p>User: {this.props.currentUser}</p> */}
+                <p>Location: Seattle, WA</p>
+                <p>Repos: 22</p>
+            </div>
+        );
+    }
+}
+
+export default InfoCard;
